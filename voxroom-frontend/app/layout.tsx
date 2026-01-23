78c07f8edb2a7cccbc83652f6b5075bbebb0dd2e
Nathanaelcpt <nathanaelchristianpt@gmail.com>
2026-01-23 16:09:53 +0700

    update UI

--- voxroom-frontend/app/layout.tsx
+++ voxroom-frontend/app/layout.tsx
@@ -1,23 +1,27 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 
-export default function Layout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <SidebarProvider>
-      <div className="flex min-h-screen w-full">
-        <AppSidebar />
+    <html lang="id">
+      <body className="min-h-screen">
+        <SidebarProvider>
+          <div className="flex min-h-screen w-full">
+            <AppSidebar />
 
-        <main className="flex-1 overflow-auto">
-          <div className="p-4">
-            <SidebarTrigger />
-            {children}
+            <main className="flex-1 overflow-auto">
+              <div className="p-4">
+                <SidebarTrigger />
+                {children}
+              </div>
+            </main>
           </div>
-        </main>
-      </div>
-    </SidebarProvider>
+        </SidebarProvider>
+      </body>
+    </html>
   )
 }
