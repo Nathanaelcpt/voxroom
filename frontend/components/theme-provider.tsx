52eea1da4be7220621cb8de0e3bd1fdbf8c239f4
Nathanaelcpt <nathanaelchristianpt@gmail.com>
2026-01-26 01:41:25 +0700

    dark mode

--- frontend/components/theme-provider.tsx
+++ frontend/components/theme-provider.tsx
@@ -5,7 +5,17 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 export function ThemeProvider({
   children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <NextThemesProvider
+      attribute="class"      // ⬅️ INI KUNCI UTAMA
+      defaultTheme="light"   // biar jelas kelihatan
+      enableSystem={false}   // matikan override OS dulu
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
