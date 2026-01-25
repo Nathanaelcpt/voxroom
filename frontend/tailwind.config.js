9f83c95ce0fd8ad4e2a559a933fc45c1367d4e4a
Nathanaelcpt <nathanaelchristianpt@gmail.com>
2026-01-26 01:18:29 +0700

    updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // ⬅️ WAJIB supaya next-themes bekerja
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
